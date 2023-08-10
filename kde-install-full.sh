91a26e4cdce62952ad519af2b5235171c037f9f6
alfonsu <90715350+alfonsu@users.noreply.github.com>
2023-08-10 13:18:39 +0300

    kde-install-full.sh

--- kde-install-full.sh
+++ kde-install-full.sh
@@ -164,8 +164,8 @@ sudo sh -c 'echo Server = https://alfonsu.github.io/\$repo/\$arch >> /etc/pacman
 ;;
 9)
 sudo pacman -Syy
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 sudo sh -c 'echo >> /etc/pacman.conf'
 sudo sh -c 'echo [chaotic-aur] >> /etc/pacman.conf'
