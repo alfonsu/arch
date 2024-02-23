d36237e9879a60eaca6ef903b13464e77c77aa47
alfonsu <90715350+alfonsu@users.noreply.github.com>
2024-02-23 08:05:05 +0200

    Add files via upload

--- alg-builder.sh
+++ alg-builder.sh
@@ -72,8 +72,8 @@ sudo sh -c 'echo Server = https://arch-linux-gui.github.io/\$repo/\$arch >> /etc
 ;;
 3)
 sudo pacman -Syy
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 sudo sh -c 'echo >> /etc/pacman.conf'
 sudo sh -c 'echo [chaotic-aur] >> /etc/pacman.conf'
