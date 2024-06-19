eb4ca5faef408a68610a87b4cb86ac2b4dcdbebf
alfonsu <90715350+alfonsu@users.noreply.github.com>
2024-06-19 11:15:45 +0300

    Add files via upload

---
+++ build-aur-package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+start (){
+sudo pacman -S git base-devel --needed --noconfirm
+menu
+}
+
+menu (){
+read -p "Enter the package name: " PACKAGE_NAME
+
+git clone https://aur.archlinux.org/$PACKAGE_NAME $HOME/aur
+cd $HOME/aur
+makepkg -src --noconfirm
+rm *debug*
+mv *zst $HOME
+rm -rf $HOME/aur
+cd $HOME
+menu
+}
+
+start
